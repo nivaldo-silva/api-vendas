d32616d0bd8c0d930997e26afc56af6fb85866c1
Nivaldo Silva <nivaldosilva.contato@gmail.com>
2026-04-08 10:54:33 -0300

    feat: implementar modelo de domínio para Categoria

---
+++ src/main/java/io/github/nivaldosilva/api_vendas/domain/Categoria.java
@@ -0,0 +1,32 @@
+package io.github.nivaldosilva.api_vendas.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "clientes")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Categoria {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private String id;
+
+    @NotBlank(message = "Nome da categoria é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+    @Indexed(unique = true)
+    @Field("nome")
+    private String nome;
+
+    @Size(max = 500, message = "Descrição deve ter no máximo 500 caracteres")
+    private String descricao;
+}
